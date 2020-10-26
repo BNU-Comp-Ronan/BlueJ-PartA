871087d2d4e1616e34a587b649f80410c8c567cb
RonanDemelo <ronandemelo23@gmail.com>
2020-10-26 15:51:33 +0000

    Created the Module class.

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,35 @@
+
+/**
+ * This calss will hold information on the modules
+ * and what percentage of the marks each module is worth to the course.
+ *
+ * @author (Ronan DeMelo)
+ * @version (26/10/2020)
+ */
+public class Module
+{
+    private String title;
+    
+    private String moduleCode;
+    
+    private int modulePercentage;
+
+    /**
+     * Constructor for objects of class Module.
+     */
+    public Module(String moduleName, String moduleID)
+    {
+        moduleName = title;
+        
+        moduleID = moduleCode;
+        
+    }
+    
+    /**
+     * Add what percentage the module is worth to the overall grade.
+     */
+    public void insertModulePercentage(int percentage)
+    {
+        percentage = 0;
+    }
+}
