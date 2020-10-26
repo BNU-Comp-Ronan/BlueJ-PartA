3da9a7364e1823f9bc4280a4db6004c60fbb51f8
RonanDemelo <ronandemelo23@gmail.com>
2020-10-26 14:58:27 +0000

    Imported the Course class from app01.

---
+++ app03-studentGrades/Course.java
@@ -0,0 +1,36 @@
+
+/**
+ * This class hold the course details.
+ *
+ * @author Ronan DeMelo
+ * @version 04/10/2020
+ */
+public class Course
+{
+    // The ID of the course.
+    private String codenumber;
+    //The Name of the course
+    private String title;
+    
+
+    /**
+     * Constructor for objects of class Course.
+     */
+    public Course(String courseName, String courseID)
+    {
+        
+        codenumber = courseID;
+        
+        title = courseName;
+        
+    }
+
+    /**
+     * Prints out the course names and IDs.
+     */
+    public void print()
+    {
+        System.out.println("Course Name:" + title + ", Course ID:" + codenumber);
+    }
+}
+
