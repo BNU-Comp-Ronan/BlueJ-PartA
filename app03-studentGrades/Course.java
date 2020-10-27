7247186192a4379006957a46a1791474bc9afc46
RonanDemelo <ronandemelo23@gmail.com>
2020-10-27 16:06:16 +0000

    Changed code in that print all modules in a course.

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -70,10 +70,10 @@ public class Course
     public void printCourseDetails()
     {
         System.out.println("Course Name:" + title + ", Course ID:" + codenumber);
-        System.out.println("Module: " + module1.title + " Module Code" + module1.moduleCode);
-        System.out.println("Module: " + module2.title + " Module Code" + module2.moduleCode);
-        System.out.println("Module: " + module3.title + " Module Code" + module3.moduleCode);
-        System.out.println("Module: " + module4.title + " Module Code" + module4.moduleCode);
+        System.out.println("Module: " + module1.title + " Module Code: " + module1.moduleCode);
+        System.out.println("Module: " + module2.title + " Module Code: " + module2.moduleCode);
+        System.out.println("Module: " + module3.title + " Module Code: " + module3.moduleCode);
+        System.out.println("Module: " + module4.title + " Module Code: " + module4.moduleCode);
     }
 
     /**
