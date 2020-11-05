6f2944aa749a74b2d5afd28fbd5177bd18b58774
RonanDemelo <ronandemelo23@gmail.com>
2020-11-05 21:41:12 +0000

    Changed code in Product sellOne(sell).

--- app04-products/Product.java
+++ app04-products/Product.java
@@ -91,11 +91,11 @@ public class Product
      * Sell one of these products.
      * An error is reported if there appears to be no stock.
      */
-    public void sellOne()
+    public void sell(int amount)
     {
-        if(quantity > 0) 
+        if(quantity >= amount) 
         {
-            quantity--;
+            quantity-= amount;
         }
         else 
         {
